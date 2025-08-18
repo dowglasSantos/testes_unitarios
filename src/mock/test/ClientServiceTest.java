efc7885ba9fbc0ef20102a210d6fd148cf50acb4
dowglasOficial <bittencourtdouglas41@gmail.com>
2025-08-18 20:26:11 -0300

    edicionando o Test errorSalvar

--- src/mock/test/ClientServiceTest.java
+++ src/mock/test/ClientServiceTest.java
@@ -1,17 +1,27 @@
 package mock.test;
 
 import mock.dao.ClientServiceMockDAO;
+import mock.dao.ClienteServiceDAO;
 import mock.services.ClienteService;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ClientServiceTest {
-    ClienteService service = new ClienteService(new ClientServiceMockDAO());
 
-    String serviceMethod = service.salvar();
 
     @Test
     public void salvar() {
+        ClienteService service = new ClienteService(new ClientServiceMockDAO());
+        String serviceMethod = service.salvar();
+
+        Assert.assertEquals("sucesso", serviceMethod);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void errorSalvar() {
+        ClienteService service = new ClienteService(new ClienteServiceDAO());
+        String serviceMethod = service.salvar();
+
         Assert.assertEquals("sucesso", serviceMethod);
     }
 }
